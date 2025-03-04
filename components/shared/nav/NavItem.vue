660c0db8dee3f45c101f7722ef4be76777a4573d
Linh Tran <52797453+thewind121212@users.noreply.github.com>
2025-03-04 23:33:34 +0700

    add the input to birth date and Gender

--- components/shared/nav/NavItem.vue
+++ components/shared/nav/NavItem.vue
@@ -11,23 +11,30 @@ const homeDashboard = defineProps<{
     active?: boolean
 }>()
 
+const navigateLink = () => {
+    if (!homeDashboard.isExpanded) return
+
+    navigateTo(homeDashboard.url)
+}
+
 </script>
 
 
 <template>
 
-    <div class="shadow-[0px_4px_6px_0px_rgba(0,0,0,0.02)] rounded-[15px] w-[14.5rem] h-[3.375rem] duration-200"
+    <NuxtLink :to="isExpanded ? url : url"  
+        class="shadow-[0px_4px_6px_0px_rgba(0,0,0,0.02)] rounded-[15px] w-[14.5rem] h-[3.375rem] duration-200"
         :class="[isExpanded ? 'hover:bg-[#1a1f37] group cursor-pointer' : 'cursor-default', active ? 'bg-[#1a1f37]' : '']">
         <div class="w-full h-full flex justify-start items-center gap-3"
             :class="isExpanded ? 'px-3 cursor-pointer' : '!px-1 cursor-default'">
-            <NuxtLink :to="url"
+            <div @click="navigateLink"
                 class="h-9 w-9 flex justify-center items-center bg-[#1A1F37] rounded-[10px] duration-200"
                 :class="[isExpanded ? 'group-hover:bg-[#0075FF] ' : 'hover:bg-[#0075ff]', active ? '!bg-[#0075ff]' : '']">
                 <NuxtImg :src="iconPath" class="w-5 h-5" />
-            </NuxtLink>
+            </div>
             <p class="text-[#FFFFFF] text-[1rem] font-sans" :class="isExpanded ? 'opacity-100' : 'opacity-0'">{{ name }}
             </p>
         </div>
-    </div>
+    </NuxtLink>
 
 </template>
